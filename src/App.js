74dbec7c34c3f743a00e8c8c97468abab98127c7
tarikyavuzca <tarikyavuzca@gmail.com>
2022-06-22 20:13:14 -0400

    finishing intro component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Intro from "./components/Intro";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Intro />
+    </div>
+  );
 }
 
 export default App;
